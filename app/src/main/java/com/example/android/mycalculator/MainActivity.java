8edf2e272c50a3b564dd9d6dbd86cd6d372b8e02
Shrey Sindher <ssindher11@gmail.com>
2019-02-12 19:35:51 +0530

    Fixed app

--- app/src/main/java/com/example/android/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/android/mycalculator/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.mycalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,15 +9,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-int result=0;
-EditText edt1;
-EditText edt2;
-Button btn1;
-Button btn2;
-Button btn3;
-Button btn4;
-Button btn5;
-TextView tv1;
+    EditText edt1;
+    EditText edt2;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    TextView tv1;
 
 
     @Override
@@ -31,34 +30,27 @@ TextView tv1;
         btn3 = findViewById(R.id.button3);
         btn4 = findViewById(R.id.button4);
         btn5 = findViewById(R.id.button5);
-        tv1= findViewById(R.id.textView1);
+        tv1 = findViewById(R.id.textView1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int num1 = Integer.parseInt(edt1.getText().toString());
+                int num2 = Integer.parseInt(edt2.getText().toString());
 
-                String str1 = edt1.getText().toString();
-                int num1 = Integer.parseInt(str1);
-
-                String str2 = edt2.getText().toString();
-                int num2 = Integer.parseInt(str2);
-
-                result = num1 + num2;
-
+                int result = num1 + num2;
+                displayOutput(result);
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str1 = edt1.getText().toString();
-                int num1 = Integer.parseInt(str1);
+                int num1 = Integer.parseInt(edt1.getText().toString());
+                int num2 = Integer.parseInt(edt2.getText().toString());
 
-                String str2 = edt2.getText().toString();
-                int num2 = Integer.parseInt(str2);
-
-                result = num1 - num2;
-
+                int result = num1 - num2;
+                displayOutput(result);
 
             }
         });
@@ -66,45 +58,29 @@ TextView tv1;
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int num1 = Integer.parseInt(edt1.getText().toString());
+                int num2 = Integer.parseInt(edt2.getText().toString());
 
-                String str1 = edt1.getText().toString();
-                int num1 = Integer.parseInt(str1);
-
-                String str2 = edt2.getText().toString();
-                int num2 = Integer.parseInt(str2);
-
-                result = num1 * num2;
+                int result = num1 * num2;
+                displayOutput(result);
             }
         });
 
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int num1 = Integer.parseInt(edt1.getText().toString());
+                int num2 = Integer.parseInt(edt2.getText().toString());
 
-                String str1 = edt1.getText().toString();
-                int num1 = Integer.parseInt(str1);
-
-                String str2 = edt2.getText().toString();
-                int num2 = Integer.parseInt(str2);
-
-                result = num1 / num2;
-
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            displayOutput(result);
-
+                int result = num1 / num2;
+                displayOutput(result);
             }
         });
     }
 
 
-    private void displayOutput(int result){
-        tv1.setText(result);
+    private void displayOutput(int result) {
+        tv1.setText(Integer.toString(result));
     }
 
 }
